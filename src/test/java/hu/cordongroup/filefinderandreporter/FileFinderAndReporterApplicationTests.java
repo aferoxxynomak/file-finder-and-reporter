a127fddec7b9ad8b89edb5f064edbe4d5cf3c5f7
Zoltan Benya <4130603+aferoxxynomak@users.noreply.github.com>
2024-03-05 15:54:22 +0100

    spring skeleton

---
+++ src/test/java/hu/cordongroup/filefinderandreporter/FileFinderAndReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cordongroup.filefinderandreporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileFinderAndReporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
